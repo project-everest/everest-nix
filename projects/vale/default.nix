4b82560d6be35355bf7d66e723d10981f688c763
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-06-02 15:04:52 +0200

    Add `runtimeconfig.json` for dotnet

--- projects/vale/default.nix
+++ projects/vale/default.nix
@@ -1,14 +1,33 @@
-{ fetchzip }:
+{ stdenv, fetchzip }:
 
 let
-
   pname = "vale";
   version = "0.3.16";
-
 in
 
-fetchzip {
+stdenv.mkDerivation rec {
   name = "${pname}-${version}";
-  url = "https://github.com/project-everest/${pname}/releases/download/v${version}/${pname}-release-${version}.zip";
-  sha256 = "0dwnw3yb9lan16m6cyzw6s9bxx3jbxydaqk16jsy1ajygj7s6rcr";
+  src = fetchzip {
+    inherit name;
+    url = "https://github.com/project-everest/${pname}/releases/download/v${version}/${pname}-release-${version}.zip";
+    sha256 = "0dwnw3yb9lan16m6cyzw6s9bxx3jbxydaqk16jsy1ajygj7s6rcr";
+  };
+  dontBuild = true;
+  dontFixup = true;
+  installPhase = ''
+    cp -r . $out
+    ls
+    for target in vale importFStarTypes; do
+      echo "$DOTNET_JSON_CONF" > $out/bin/$target.runtimeconfig.json
+    done
+  '';
+  DOTNET_JSON_CONF = ''
+    {
+      "runtimeOptions": {
+        "framework": {
+          "name": "Microsoft.NETCore.App",
+          "version": "6.0.0"
+        }
+      }
+    }'';
 }

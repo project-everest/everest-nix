36fec12d248e6922aef4533f8a53a1a3878fcdf3
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-06-03 08:00:00 +0200

    update job to distribute hacl build products

--- projects/hacl/default.nix
+++ projects/hacl/default.nix
@@ -1,54 +1,75 @@
 { enableParallelBuilding ? true, dotnet-runtime, ocamlPackages, python3, stdenv
 , which, time, z3, fstar, karamel, vale, mlcrypto, src }:
 
-stdenv.mkDerivation {
-  name = "hacl-star";
+let
 
-  inherit src;
+  hacl = stdenv.mkDerivation {
+    name = "hacl-star";
+  
+    inherit src;
+  
+    postPatch = ''
+      patchShebangs tools
+      patchShebangs dist/configure
+      substituteInPlace Makefile --replace "/usr/bin/time" "`which time`"
+      substituteInPlace Makefile --replace "NOSHORTLOG=1" ""
+    '';
+  
+    nativeBuildInputs = [ z3 python3 which dotnet-runtime time ]
+      ++ (with ocamlPackages; [
+        ocaml
+        findlib
+        batteries
+        pprint
+        stdint
+        yojson
+        zarith
+        ppxlib
+        ppx_deriving
+        ppx_deriving_yojson
+        ctypes
+      ]);
+  
+    MLCRYPTO_HOME = mlcrypto;
+    VALE_HOME = vale;
+    FSTAR_HOME = fstar;
+    KRML_HOME = karamel;
+  
+    configurePhase = ''
+      export HACL_HOME=$(pwd)
+    '';
+  
+    inherit enableParallelBuilding;
+  
+    preBuild = ''
+      rm -rf dist/*/*
+      rm -rf hints/*
+    '';
+  
+    buildTargets = [ "ci" ];
+  
+    installPhase = ''
+      cp -r ./. $out
+    '';
+  
+    dontFixup = true;
 
-  postPatch = ''
-    patchShebangs tools
-    patchShebangs dist/configure
-    substituteInPlace Makefile --replace "/usr/bin/time" "`which time`"
-    substituteInPlace Makefile --replace "NOSHORTLOG=1" ""
-  '';
+    passthru.build-products = stdenv.mkDerivation {
+      name = "hacl-build-products";
+      phases = [ "installPhase" ];
+      installPhase = ''
+        mkdir -p $out
+        cd ${hacl}
+        tar -cvf $out/hints.tar hints
+        tar -cvf $out/dist.tar dist/*/*
+  
+        mkdir -p $out/nix-support
+        echo "file hints $out/hints.tar" >> $out/nix-support/hydra-build-products
+        echo "file dist $out/dist.tar" >> $out/nix-support/hydra-build-products
+      '';
+    };
+  };
 
-  nativeBuildInputs = [ z3 python3 which dotnet-runtime time ]
-    ++ (with ocamlPackages; [
-      ocaml
-      findlib
-      batteries
-      pprint
-      stdint
-      yojson
-      zarith
-      ppxlib
-      ppx_deriving
-      ppx_deriving_yojson
-      ctypes
-    ]);
+in
 
-  MLCRYPTO_HOME = mlcrypto;
-  VALE_HOME = vale;
-  FSTAR_HOME = fstar;
-  KRML_HOME = karamel;
-
-  configurePhase = ''
-    export HACL_HOME=$(pwd)
-  '';
-
-  inherit enableParallelBuilding;
-
-  preBuild = ''
-    rm -rf dist/*/*
-    rm -rf hints/*
-  '';
-
-  buildTargets = [ "ci" ];
-
-  installPhase = ''
-    cp -r ./. $out
-  '';
-
-  dontFixup = true;
-}
+hacl
